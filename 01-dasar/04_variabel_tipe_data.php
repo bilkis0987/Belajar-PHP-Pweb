edd3e5a2d5fcf298fdfb260b40df39147520530e
unknown <2425286.bilkis@smkn-2sbg.sch.id>
2025-11-03 11:35:16 +0700

    menambahkan materi file praktek variabel tipe data dan penjelasan tentang cara mendeklarasikan variabel dan berbagai tipe data dalam PHP

---
+++ 01-dasar/04_variabel_tipe_data.php
@@ -0,0 +1,25 @@
+<?php
+
+// 1. Sintaks Dasar & Variabel
+
+// Tipe Data String
+$nama = "blikis";
+
+// Tipe Data Integer
+$umur = 17;
+
+// Tipe Data Float
+$nilai = 95.5;
+
+// Tipe Data Boolean
+$apakah_siswa = true;
+
+echo "Nama: " . $nama;
+echo "<br>";
+echo "Umur: " . $umur;
+echo "<br>";
+echo "Nilai: " . $nilai;
+echo "<br>";
+echo "siswa: " . ($apakah_siswa ? 'Ya' : 'Bukan');
+
+?>
