cc2a338dd3213c6b2a944c3f5b955a368b8acfb4
unknown <2425286.bilkis@smkn-2sbg.sch.id>
2025-11-03 11:35:12 +0700

    menambahkan materi file praktek komentar dan penjelasan tentang cara menulis komentar dalam PHP menggunakan //, #, dan /* */

---
+++ 01-dasar/02_komentar.php
@@ -0,0 +1,21 @@
+<?php
+
+// Ini adalah komentar satu baris (single-line comment).
+// Komentar tidak akan dieksekusi oleh program dan hanya berguna untuk programmer.
+
+echo "Contoh penulisan komentar di PHP.";
+
+# Ini juga komentar satu baris, gaya shell-script.
+echo "<br>"; // Komentar juga bisa diletakkan di akhir baris.
+
+/*
+  Ini adalah komentar
+  yang terdiri dari
+  beberapa baris (multi-line comment).
+  Semua teks di antara /* dan  * / akan diabaikan
+  // oleh interpreter PHP.
+*/
+
+echo "Komentar sangat penting untuk membuat kode lebih mudah dibaca.";
+
+?>
