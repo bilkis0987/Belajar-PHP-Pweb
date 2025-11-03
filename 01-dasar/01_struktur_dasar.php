d8a8ef50bb2cd512a60aca8ae9ead286cb46ea9b
unknown <2425286.bilkis@smkn-2sbg.sch.id>
2025-11-03 11:35:10 +0700

    menambahkan materi file praktek struktur dasar dan penjelasan tentang struktur dasar PHP seperti tag pembuka dan penutup, serta cara menulis kode PHP

---
+++ 01-dasar/01_struktur_dasar.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ Ini adalah struktur program PHP yang paling dasar.
+ Semua kode PHP harus berada di dalam tag <?php ... ?>
+*/
+
+// Perintah untuk menampilkan teks "Hello, World!"
+
+echo "Hello, World!";
+
+// Akhir dari blok PHP (tag penutup ? > opsional jika file hanya berisi kode PHP)
+?>
